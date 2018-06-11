961ad120b5fb92db98578907d4456cd8cd1c8014
wilsonfreitas <wilson.freitas@gmail.com>
2018-06-11 06:21:25 -0300

    Functions to import and export calendar
    load_calendar imports calendar from json files
    save_calendar exports calendar to json files

---
+++ R/calendar-export.R
@@ -0,0 +1,63 @@
+
+cleanup_cal_list <- function(x) {
+  x <- Filter(length, x)
+  Filter(Negate(is.null), x)
+}
+
+handle_adjust <- function(x) {
+  switch (x,
+          adjust.next = "following",
+          adjust.previous = "preceding",
+          adjust.none = NULL
+  )
+}
+
+cal2list <- function(cal) {
+  cal_list <- list(
+    name = cal$name,
+    weekdays = cal$weekdays,
+    holidays = cal$holidays,
+    financial = cal$financial,
+    adjust.from = handle_adjust(cal$adjust.from_label),
+    adjust.to = handle_adjust(cal$adjust.to_label)
+  )
+  cleanup_cal_list(cal_list)
+}
+
+#' @export
+save_calendar <- function(cal, con) {
+  cal_list <- cal2list(cal)
+  writeLines(jsonlite::toJSON(cal_list, auto_unbox = TRUE, pretty = TRUE), con)
+}
+
+#' @export
+load_calendar <- function(con) {
+  x <- jsonlite::fromJSON(con)
+  adjust_from <- switch (if (is.null(x$adjust.from)) "none" else x$adjust.from,
+                         none = adjust.none,
+                         following = adjust.next,
+                         preceding = adjust.previous
+  )
+  adjust_to <- switch (if (is.null(x$adjust.to)) "none" else x$adjust.to,
+                       none = adjust.none,
+                       following = adjust.next,
+                       preceding = adjust.previous
+  )
+  holidays <- if (is.null(x$holidays) || length(x$holidays) == 0)
+    integer()
+  else
+    as.Date(x$holidays)
+  cal <- create.calendar(
+    x$name,
+    holidays,
+    weekdays = x$weekdays, # nullable
+    start.date = x$start.date, # nullable
+    end.date = x$end.date, # nullable
+    adjust.from = adjust_from,
+    adjust.to = adjust_to,
+    financial = if (is.null(x$financial)) FALSE else x$financial
+  )
+  invisible(cal)
+}
+
+print(load_calendar("inst/extdata/weekends.json"))
