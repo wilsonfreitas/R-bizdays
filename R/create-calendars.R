19e8c571c25f5d1d3828e1bc4d471ba0805fe57c
wilsonfreitas <wilson.freitas@gmail.com>
2022-01-30 16:30:21 -0300

    Moved .onAttach to zzz.R

--- R/create-calendars.R
+++ R/create-calendars.R
@@ -121,23 +121,6 @@
 #' }
 NULL
 
-# Registering calendars
-
-.onAttach <- function(libname, pkgname) {
-  create.calendar("actual")
-  localenv <- new.env()
-  utils::data("holidaysANBIMA", envir = localenv)
-  create.calendar("Brazil/ANBIMA", localenv$holidaysANBIMA,
-                  weekdays = c('saturday', 'sunday'),
-                  adjust.from = adjust.next, adjust.to = adjust.previous)
-  utils::data("holidaysB3", envir = localenv)
-  create.calendar("Brazil/B3", localenv$holidaysB3,
-                  weekdays = c('saturday', 'sunday'),
-                  adjust.from = adjust.next, adjust.to = adjust.previous)
-  create.calendar("weekends", weekdays = c('saturday', 'sunday'),
-                  adjust.from = adjust.next, adjust.to = adjust.previous)
-  bizdays.options$set(default.calendar = 'actual')
-}
 
 #' @rdname other-calendars
 #' @export
---
+++ R/zzz.R
@@ -0,0 +1,18 @@
+
+# Registering calendars
+
+.onAttach <- function(libname, pkgname) {
+  create.calendar("actual")
+  localenv <- new.env()
+  utils::data("holidaysANBIMA", envir = localenv)
+  create.calendar("Brazil/ANBIMA", localenv$holidaysANBIMA,
+                  weekdays = c('saturday', 'sunday'),
+                  adjust.from = adjust.next, adjust.to = adjust.previous)
+  utils::data("holidaysB3", envir = localenv)
+  create.calendar("Brazil/B3", localenv$holidaysB3,
+                  weekdays = c('saturday', 'sunday'),
+                  adjust.from = adjust.next, adjust.to = adjust.previous)
+  create.calendar("weekends", weekdays = c('saturday', 'sunday'),
+                  adjust.from = adjust.next, adjust.to = adjust.previous)
+  bizdays.options$set(default.calendar = 'actual')
+}
